2f9d9afb857d2bb0b4b658a82d3c6df8d71c982e
rich5580 <rich5580@mylaurier.ca>
2021-10-04 17:04:47 -0400

    Commit

---
+++ app/src/main/java/com/example/budgetbuddy/RegisterContActivity.java
@@ -0,0 +1,14 @@
+package com.example.budgetbuddy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterContActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_cont);
+    }
+}
