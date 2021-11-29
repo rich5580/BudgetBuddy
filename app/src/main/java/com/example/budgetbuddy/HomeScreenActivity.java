78cc6a0834ce42b48ac6a467dfcc2405bde012ca
rich5580 <rich5580@mylaurier.ca>
2021-11-29 16:32:19 -0500

    Added stuff to the HomeScreenActivity

--- app/src/main/java/com/example/budgetbuddy/HomeScreenActivity.java
+++ app/src/main/java/com/example/budgetbuddy/HomeScreenActivity.java
@@ -1,10 +1,14 @@
 package com.example.budgetbuddy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class HomeScreenActivity extends AppCompatActivity {
 
@@ -19,5 +23,34 @@ public class HomeScreenActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.directorymain, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logout:
+                Intent intent = new Intent(HomeScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+                Toast toast = Toast.makeText(HomeScreenActivity.this, "You have logged out.", Toast.LENGTH_LONG);
+                toast.show();
+                return true;
+            case R.id.details:
+                Intent intent2 = new Intent(HomeScreenActivity.this, DetailsPageActivity.class);
+                startActivity(intent2);
+                return true;
+            case R.id.enterData:
+                Intent intent3 = new Intent(HomeScreenActivity.this, EnterDataActivity.class);
+                startActivity(intent3);
+                return true;
+            case R.id.breakdown:
+                Intent intent4 = new Intent(HomeScreenActivity.this, BreakdownPageActivity.class);
+                startActivity(intent4);
+                return true;
+            case R.id.settingsPage:
+                Intent intent5 = new Intent(HomeScreenActivity.this, SettingPageActivity.class);
+                startActivity(intent5);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/budgetbuddy/MainActivity.java
+++ app/src/main/java/com/example/budgetbuddy/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.budgetbuddy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        UserDatabaseHelper helper = new UserDatabaseHelper(this);
+        SQLiteDatabase db = helper.getWritableDatabase();
+
         email = (EditText) findViewById(R.id.et_Email);
         password = (EditText) findViewById(R.id.et_Password);
 
@@ -27,16 +32,34 @@ public class MainActivity extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String retrieve_email = "";
+                String retrieve_pw = "";
                 mail = String.valueOf(email.getText());
                 pw = String.valueOf(password.getText());
                 Intent intent = new Intent(MainActivity.this, HomeScreenActivity.class);
 
+                Cursor c = db.rawQuery("Select email,pswd from users where email= ?", new String[]{mail});
+                c.moveToFirst();
+                if (c.getCount() == 0) {
+                    Toast toast = Toast.makeText(MainActivity.this, "Email and/or password does not exist.", Toast.LENGTH_LONG);
+                    toast.show();
+                } else {
+                    retrieve_email = c.getString(0);
+                    retrieve_pw = c.getString(1);
+                }
+
                 if (mail.equals("") || pw.equals("")) {
                     Toast toast = Toast.makeText(MainActivity.this, "Please enter email and/or password before you can login.", Toast.LENGTH_LONG);
                     toast.show();
-                } else {
+                }
+                else if (retrieve_email.equals(mail) && retrieve_pw.equals(pw)) {
                     startActivity(intent);
                 }
+                else {
+                    Toast toast = Toast.makeText(MainActivity.this, "Email and/or password does not exist.", Toast.LENGTH_LONG);
+                    toast.show();
+                }
+                c.close();
 
             }
         });
